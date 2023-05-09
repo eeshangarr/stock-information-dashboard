cefb709d2e900030f63620fd2d60b874d427efd7
eeshangarr <eeshangarr@gmail.com>
2023-05-09 17:07:58 -0500

    Set up backend and frontend folders and connected frontend and backend

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+from flask_cors import cross_origin
+
+app = Flask(__name__)
+
+@app.route("/tickerSymbol", methods = ["POST", "OPTIONS"])
+@cross_origin()
+def tickerSymbol():
+    if request.method == "POST":
+        print("Ticker Symbol: " + str(request.json.get("tickerSymbol")))
+    return "200", 200
